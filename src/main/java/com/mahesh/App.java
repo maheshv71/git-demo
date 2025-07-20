2294d47e7f65984de662c618aa0c4cfe4e1dc992
mahesh <mahesh.vemulapally@gmail.com>
2025-07-20 17:47:23 +0530

    added if condition to check  the data

--- src/main/java/com/mahesh/App.java
+++ src/main/java/com/mahesh/App.java
@@ -5,6 +5,10 @@ public class App {
 	public void addData(String data)
 	{
 		System.out.println("data"+data);
+		if(data.equals("mahesh"))
+		{
+			System.out.println("true");
+		}
 	}
 	
 	public static void main(String[] args) {
