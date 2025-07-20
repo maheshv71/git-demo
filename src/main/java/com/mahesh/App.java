fe4cb3a1731076f41b89cfec55002c1f83a51fd3
mahesh <mahesh.vemulapally@gmail.com>
2025-07-20 17:45:14 +0530

    added git-demo projet to local repo

---
+++ src/main/java/com/mahesh/App.java
@@ -0,0 +1,15 @@
+package com.mahesh;
+
+public class App {
+
+	public void addData(String data)
+	{
+		System.out.println("data"+data);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
